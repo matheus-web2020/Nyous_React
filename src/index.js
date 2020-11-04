68a3a313faf631ef15ca0147694bbf1178ba9e4b
Matheus <matheus.fusca.lima@outlook.com>
2020-11-04 16:26:57 -0300

    Parte 2 Pronta

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import Home from './pages/home';
 import Login from './pages/login';
 import Cadastrar from './pages/cadastrar';
+import Eventos from './pages/eventos';
 import NaoEncontrada from './pages/naoencontrada';
 import reportWebVitals from './reportWebVitals';
+
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import DashBoard from './pages/admin/dashboard';
 
 
 const routing = (
@@ -18,6 +21,8 @@ const routing = (
       <Route exact path='/' component={Home}/>
       <Route path='/login' component={Login}/>
       <Route path='/cadastrar' component={Cadastrar}/>
+      <Route path='/eventos' component={Eventos}/>
+      <Route path='/admin/dashboard' component={DashBoard}/>
       <Route component={NaoEncontrada}/>
     </Switch>
   </Router>
